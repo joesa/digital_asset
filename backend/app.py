fec8cefe75000915680a8c05d4b0f5201c7a92a2
Joseph Sintim-Amoah <joesa73@gmail.com>
2024-10-02 00:16:38 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # MongoDB connection setup
-client = MongoClient('mongodb+srv://joesa73:<db_password>@digitalassets.9rfsg.mongodb.net/?retryWrites=true&w=majority&appName=digitalassets')
+client = MongoClient('mongodb+srv://joesa73:22F2aOMvjsWmVNaF@digitalassets.9rfsg.mongodb.net/?retryWrites=true&w=majority&appName=digitalassets')
 db = client['service_requests_db']
 collection = db['requests']
 
