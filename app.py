cd7ceb115047432e8f93b245ca1048a154d259fa
Joseph Sintim-Amoah <joesa73@gmail.com>
2024-10-03 00:35:40 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+uri = "mongodb+srv://joesa73:wq3DNdfxDtJKv6@digitalassets.9rfsg.mongodb.net/?retryWrites=true&w=majority&appName=digitalassets"
+
+# MongoDB connection setup
+client = MongoClient(uri)
+db = client['service_requests_db']
+collection = db['requests']
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    data = request.get_json()
+    
+    # Validate data
+    if not all(k in data for k in ('name', 'phone', 'email', 'serviceType')):
+        return jsonify({'error': 'Missing data fields'}), 400
+    
+    # Store data in MongoDB
+    collection.insert_one({
+        'name': data['name'],
+        'phone': data['phone'],
+        'email': data['email'],
+        'service_type': data['serviceType']
+    })
+    
+    return jsonify({'message': 'Form submitted successfully!'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
